c949ea2be55e1ead64af554a13e52dda67af963b
StoilTomanov <stoiltomanov@gmail.com>
2023-12-29 17:31:32 +0200

    add dropdown options

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -11,9 +11,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -22,8 +25,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -50,28 +55,48 @@ fun UnitConverter() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Unit Converter")
-        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Unit Converter", fontSize = 20.sp, fontWeight = FontWeight(800))
+        Spacer(modifier = Modifier.height(30.dp))
         OutlinedTextField(value = "", onValueChange = {
 
         })
         Row {
             Box {
                 Button(onClick = { /*TODO*/ }) {
-                    Text(text = "Select")
+                    Text(text = "Select", fontSize = 16.sp)
                     Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = "Button")
                 }
+                DropdownMenu(expanded = false, onDismissRequest = {}) {
+                    DropdownMenuItem(
+                        text = { Text(text = "Centimeters", fontSize = 16.sp) },
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Meters", fontSize = 16.sp) },
+                        onClick = { /*TODO*/ }
+                    )
+                }
             }
-
+            Spacer(modifier = Modifier.width(20.dp))
             Box {
                 Button(onClick = { /*TODO*/ }) {
-                    Text(text = "Select")
+                    Text(text = "Select", fontSize = 16.sp)
                     Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = "Button")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = {}) {
+                    DropdownMenuItem(
+                        text = { Text(text = "Meters", fontSize = 16.sp) },
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Centimeters", fontSize = 16.sp) },
+                        onClick = { /*TODO*/ }
+                    )
                 }
             }
         }
         Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Result:")
+        Text(text = "Result:", fontSize = 16.sp)
     }
 }
 
